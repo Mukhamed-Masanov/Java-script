6d89f950e86bd722ffb76424a960d50880695454
Mukhamed Masaov <masanovmukhamed075@gmail.com>
2021-04-29 21:24:58 +0700

    java script lesson 5

---
+++ urok3/js/app.js
@@ -0,0 +1,11 @@
+// let a = +prompt('Введите число');
+// let c = a**2;
+// alert(c);
+// let a = +prompt("Number 1");
+// let b = +prompt("Number2");
+// let c = a + b;
+// let c1 = a - b;
+// let c2= a *b;
+// let c3= a/b;
+// alert(`результат сложения ${c} результат вычетания ${c1} результат умножения ${c2}  результат деления ${c3}`)
+
